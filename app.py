aff9af4159fa92703bd6f0850e7d1d8189dde141
NAYAN-2006 <nayanroy2006@gmail.com>
2026-08-31 10:59:27 +0400

    Fix add function for tests

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def add(a, b):
+    return a + b
+
+
 @app.route("/")
 def home():
     return "Hello from Dockerized Flask App!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
